aba5cd88571de538a998c74b1bdf7d38687b6c8d
iamkuaileshizhe <kuaileshizhe_01@126.com>
2018-07-05 22:05:24 +0800

    1

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -3,16 +3,17 @@ package com.example.administrator.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_click ;
     TextView txt_show;
-    Button btn_click1 ;
-    TextView txt_show1;
 
 
     @Override
@@ -32,9 +33,11 @@ public class MainActivity extends AppCompatActivity {
         btn_click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"你好啊 ！我是神",Toast.LENGTH_LONG).show();
+                Log.i("d","h啊哈哈哈");
                 txt_show.setText("哈哈哈，我变了");
-                Intent intent  = new Intent(MainActivity.this,TestActivity.class);
-                startActivity(intent);
+//                Intent intent  = new Intent(MainActivity.this,TestActivity.class);
+//                startActivity(intent);
             }
         });
         txt_show.setOnLongClickListener(new View.OnLongClickListener() {
